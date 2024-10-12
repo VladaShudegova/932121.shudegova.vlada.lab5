e02cc032821803df486d207e616b564f33021ed6
Влада Шудегова <v.k.shudegova@viarune.com>
2024-10-12 23:31:32 +0700

    Finished lab 5

---
+++ script.js
@@ -0,0 +1,12 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
